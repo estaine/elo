5eef824b5355597c43ffccf86bbeabdbaa34d4ed
Ruslan Molchanov <ruslanys@gmail.com>
2017-09-21 13:47:30 +0300

    .

--- src/main/java/com/estaine/elo/Application.java
+++ src/main/java/com/estaine/elo/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EloApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
