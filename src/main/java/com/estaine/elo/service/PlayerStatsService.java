aadb799d6e82574c2a9f085986125ab3d9762785
Ruslan Molchanov <ruslanys@gmail.com>
2017-09-21 13:47:03 +0300

    .

--- src/main/java/com/estaine/elo/service/PlayerStatsService.java
+++ src/main/java/com/estaine/elo/service/PlayerStatsService.java
@@ -3,5 +3,7 @@ package com.estaine.elo.service;
 import com.estaine.elo.entity.PlayerStats;
 
 public interface PlayerStatsService {
+
     PlayerStats getPlayerStats(String username);
+
 }
--- src/main/java/com/estaine/elo/service/RatingService.java
+++ src/main/java/com/estaine/elo/service/RatingService.java
@@ -2,8 +2,11 @@ package com.estaine.elo.service;
 
 import com.estaine.elo.entity.Player;
 import com.estaine.elo.entity.PlayerStats;
+
 import java.util.Map;
 
 public interface RatingService {
+
     Map<Player, PlayerStats> calculateRatings();
+
 }
