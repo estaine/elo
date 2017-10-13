231caedd90421935f8155acd677c66fcc9709e33
Andrey Rykhalski <andrey.rykhalski@dmetrics.com>
2017-10-13 18:03:05 +0300

    Explicit bean definition removed as redundant

--- src/main/java/com/estaine/elo/Application.java
+++ src/main/java/com/estaine/elo/Application.java
@@ -2,23 +2,12 @@ package com.estaine.elo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ViewResolver getViewResolver() {
-
-        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-        return resolver;
-
     }
 
 }
